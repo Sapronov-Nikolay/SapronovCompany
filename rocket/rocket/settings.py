460d0eac68a43804df49396fe23f61a259135625
Sapronov-Nikolay <avindialit@list.ru>
2023-11-23 06:20:47 +0300

    Внесены изменения для будущих опций и настроек в sttings

--- rocket/rocket/settings.py
+++ rocket/rocket/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'rocket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'todo_service',
+        }
+        # Старая запись сохранена на всякий случай 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -108,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru' # оставим запись на всякий случай 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -120,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
